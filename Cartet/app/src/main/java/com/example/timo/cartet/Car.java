d7ae76b44cc48ad63182cf02a2ffcf805aa99fbe
Timo <timodebimo@hotmail.com>
2018-03-29 14:25:08 +0200

    Spotting fully functional with acquiring data from RDW

--- Cartet/app/src/main/java/com/example/timo/cartet/Car.java
+++ Cartet/app/src/main/java/com/example/timo/cartet/Car.java
@@ -5,4 +5,89 @@ package com.example.timo.cartet;
  */
 
 class Car {
+    private String aantalCilinders;
+    private String aantalZitplaatsen;
+    private String cilinderInhoud;
+    private String kleur;
+    private String handelsBenaming;
+    private String voertuigType;
+    private String kenteken;
+    private String lengte;
+    private String massa;
+    private String merk;
+    private String voertuigSoort;
+
+
+    public String getAantalCilinders() {
+        return aantalCilinders;
+    }
+
+    public String getAantalZitplaatsen() {
+        return aantalZitplaatsen;
+    }
+
+    public String getCilinderInhoud() {
+        return cilinderInhoud;
+    }
+
+    public String getKleur() {
+        return kleur;
+    }
+
+    public String getHandelsBenaming() {
+        return handelsBenaming;
+    }
+
+    public String getVoertuigType() {
+        return voertuigType;
+    }
+
+    public String getKenteken() {
+        return kenteken;
+    }
+
+    public String getLengte() {
+        return lengte;
+    }
+
+    public String getMassa() {
+        return massa;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public String getVoertuigSoort() {
+        return voertuigSoort;
+    }
+
+    public Car(String aantalCilinders, String aantalZitplaatsen, String cilinderInhoud, String kleur,
+               String handelsBenaming, String voertuigType, String kenteken, String lengte,
+               String massa, String merk, String voertuigSoort) {
+        this.aantalCilinders = aantalCilinders;
+        this.aantalZitplaatsen = aantalZitplaatsen;
+        this.cilinderInhoud = cilinderInhoud;
+        this.kleur = kleur;
+        this.handelsBenaming = handelsBenaming;
+        this.voertuigType = voertuigType;
+        this.kenteken = kenteken;
+        this.lengte = lengte;
+        this.massa = massa;
+        this.merk = merk;
+        this.voertuigSoort = voertuigSoort;
+    }
+
+    public String ToString(){
+        return "Kenteken: " + getKenteken() + "\n"
+                + "Merk: " + getMerk() + "\n"
+                + "Voertuigsoort: " + getVoertuigSoort() + "\n"
+                + "Voertuigtype: " + getVoertuigType() + "\n"
+                + "Kleur: " + getKleur() + "\n"
+                + "Serie: " + getHandelsBenaming() + "\n"
+                + "Cilinderinhoud: " + getCilinderInhoud() + "\n"
+                + "Aantal cilinders: " + getAantalCilinders() + "\n"
+                + "Lengte: " + getLengte() + "\n"
+                + "Massa: " + getMassa();
+    }
 }
--- Cartet/app/src/main/java/com/example/timo/cartet/RDWActivity.java
+++ Cartet/app/src/main/java/com/example/timo/cartet/RDWActivity.java
@@ -8,8 +8,10 @@ import android.util.Log;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -61,36 +63,70 @@ public class RDWActivity extends AppCompatActivity {
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
-                progressBar.setVisibility(progressBar.VISIBLE);
+                //progressBar.setVisibility(progressBar.VISIBLE);
 
                 try{
-                    rdwURL = new URL("https://opendata.rdw.nl/resource/m9d7-ebf2.json"
-                            + "?kenteken=" + licence);
+                    rdwURL = new URL("https://opendata.rdw.nl/resource/m9d7-ebf2.json?"
+                            + "$$app_token=" + appToken
+                            + "&kenteken=" + licence);
                 }
                 catch (MalformedURLException e){
                     Log.e("MalformedURL", "Can't connect to the RDW..");
                 }
 
                 try{
-                    HttpsURLConnection myConnection = (HttpsURLConnection) rdwURL.openConnection();
-                    myConnection.setRequestProperty("X-App-Token", appToken);
+                    HttpURLConnection myConnection = (HttpsURLConnection) rdwURL.openConnection();
                     myConnection.connect();
 
                     if (myConnection.getResponseCode() == 200) {
                         //connection established, start reading
-                        textViewBrand.setText("Trying to read");
-                       
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(myConnection.getInputStream()));
+                        StringBuilder stringBuilder = new StringBuilder();
+                        String line;
+                        while ((line = bufferedReader.readLine()) != null) {
+                            stringBuilder.append(line).append("\n");
+                        }
+                        bufferedReader.close();
+                        Car car = parseDataToClass(stringBuilder.toString());
+                        textViewBrand.setText(car.ToString());
+                        //progressBar.setVisibility(progressBar.GONE);
+                        myConnection.disconnect();
                     }
                     else {
                         textViewBrand.setText("ERROR");
                     }
                 }
                 catch(Exception e){
-                    Log.e("ERROR", "Something went wrong..");
+                    e.printStackTrace();
                 }
             }
         });
 
         return null;
+    }
+
+    private Car parseDataToClass(String allData){
+        Car car = new Car(getValueFromKey(allData, "\"aantal_cilinders\""),
+                getValueFromKey(allData, "\"aantal_zitplaatsen\""),
+                getValueFromKey(allData, "\"cilinderinhoud\""),
+                getValueFromKey(allData, "\"eerste_kleur\""),
+                getValueFromKey(allData, "\"handelsbenaming\""),
+                getValueFromKey(allData, "\"inrichting\""),
+                getValueFromKey(allData, "\"kenteken\""),
+                getValueFromKey(allData, "\"lengte\""),
+                getValueFromKey(allData, "\"massa_ledig_voertuig\""),
+                getValueFromKey(allData, "\"merk\""),
+                getValueFromKey(allData, "\"voertuigsoort\""));
+
+        return car;
+    }
+
+    private String getValueFromKey(String allData, String key){
+        int indexOfKey = allData.indexOf(key);
+        int indexOfValue = allData.indexOf(":", indexOfKey);
+        int endIndexOfValue = allData.indexOf(",", indexOfValue);
+        String value = allData.substring(indexOfValue + 2, endIndexOfValue - 1);
+
+        return value;
     }
 }
